6621db6d49153d57bf0369d91c3638ace2618628
Lautaro Marguery <30993136+ARESLautaro95estudio@users.noreply.github.com>
2024-06-12 17:36:30 -0300

    Add files via upload

---
+++ Subir a git/index.js
@@ -0,0 +1,11 @@
+console.log("Entro al index de java");
+
+
+
+function getFromApi()
+{
+    return [
+        {id : 1, img : "" ,alt :"", title :"",text },
+        {id : 2, img : "" ,alt :"", title :"",text }
+    ]
+}
